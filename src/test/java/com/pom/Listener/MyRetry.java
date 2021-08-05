05ffc6dad415636721600f3ac6e3152fec3fe7ba
pushpak wani <wanipushpak71@gmail.com>
2021-08-05 12:52:04 +0530

    retry

---
+++ src/test/java/com/pom/Listener/MyRetry.java
@@ -0,0 +1,19 @@
+package com.pom.Listener;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+ 
+public class MyRetry implements IRetryAnalyzer {
+ 
+  private int retryCount = 0;
+  private static final int maxRetryCount = 10;
+ 
+
+  public boolean retry(ITestResult result) {
+    if (retryCount < maxRetryCount) {
+      retryCount++;
+      return true;
+    }
+    return false;
+  }
+}
--- src/test/java/com/pom/pages/Test3.java
+++ src/test/java/com/pom/pages/Test3.java
@@ -1,13 +1,15 @@
 package com.pom.pages;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test3 {
 	
-	@Test
+	@Test(retryAnalyzer=com.pom.Listener.MyRetry.class)
 	public void test5() {
 		
 		System.out.println("hello");
+		
 		
 	}
 
